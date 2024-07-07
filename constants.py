3a324ad3ee13e1a18adcbf4191fb18287399e6f3
Mahadev Gaonkar <mahadevgaonkar@Mahadevs-MacBook-Pro.local>
2024-07-07 16:16:52 -0700

    minor bug fixes

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
-DATASET_FILE = "database/final_data.csv"
+DATASET_FILE = "database/movies_with_poster.csv"
 DEFAULT_CONFIF_FILE = "config.json"
 EMBEDDING_MODEL = "all-MiniLM-L6-v2.gguf2.f16.gguf"
 CHUNK_SIZE = 1000
@@ -7,3 +7,4 @@ NO_DOCS_PER_QUERY = 20
 MAX_DOCS_TO_LOAD = 100
 
 NUM_COLUMNS = 5
+DEFAULT_MOVIE_POSTER = "https://m.media-amazon.com/images/M/MV5BZmQ4MzZlZTAtNDRhMC00MTQ2LTkyN2YtNTQ1ZTliM2UwMWNhXkEyXkFqcGdeQXVyMTQ2MjQyNDc@._V1_.jpg"
--- frontend/gui.py
+++ frontend/gui.py
@@ -17,6 +17,13 @@ class GUI():
         self.dataset = None
         self.num_cols = constants.NUM_COLUMNS
 
+    def print_movie_names(self, movie_data):
+        """Print the movie names."""
+        for movie in movie_data:
+            match = re.search(r"noriginal_title:\s+(.*)\n", movie)
+            if match:
+                logger.info(f"Movie: {match.group(1)}")
+
     def get_similar_content(self, search_query):
             """Get the context of the search."""
             doc_list = []
@@ -36,7 +43,7 @@ class GUI():
             match = re.search(r"PosterLink:\s+(.*)", movie)
             if match:
                 poster_images.append(match.group(1))
-            match = re.search(r"Name:\s+(.*)", movie)
+            match = re.search(r"original_title:\s+(.*)", movie)
             if match:
                 logger.info(f"Movie: {match.group(1)}")
         self.update_movie_posters(poster_images)
@@ -60,16 +67,18 @@ class GUI():
         with st.container(border=True):
             search_col1, search_col2 = st.columns([4, 1], gap="small", vertical_alignment="center")
             with search_col1:
-                search_query = st.text_input("", "horror movies with zombies", label_visibility="collapsed")
+                search_query = st.text_input("", "horror movies with zombies", label_visibility="collapsed", key="search_query")
 
             with search_col2:
                 search_button = st.button("Search")
 
         if search_button:
             st.write(f"Searching movies...")
-            similar_movies = self.get_similar_content(search_query)
-            # logger.info(f"similar_movies: {similar_movies}")
-            self.update_movie_recommendations(similar_movies)
+            if "search_query" in st.session_state:
+                similar_movies = self.get_similar_content(st.session_state.search_query)
+                # logger.info(f"similar_movies: {similar_movies}")
+                self.print_movie_names(similar_movies)
+                self.update_movie_recommendations(similar_movies)
 
         self.poster_container = st.empty()
         st.session_state.poster_container = self.poster_container
@@ -88,5 +97,8 @@ class GUI():
                 for col in cols:
                     if index >= len(image_paths):
                         break
-                    col.image(image_paths[index], use_column_width=True)
+                    if image_paths[index] == "":
+                        col.image(constants.DEFAULT_MOVIE_POSTER, use_column_width=True)
+                    else:
+                        col.image(image_paths[index], use_column_width=True)
                     index = index + 1
