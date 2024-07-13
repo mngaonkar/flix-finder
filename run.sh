ab64202cfd565d81fac6d292fee2b6c1355ebc56
mahadev <mahadev.gaonkar@gmail.com>
2024-07-13 17:38:04 +0000

    fixed starter script

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
-streamlit run --ui.hideTopBar True --client.toolbarMode viewer main.py --server.port 80
+cd /root/code/flix-finder
+/root/miniconda3/bin/conda run -n langchain streamlit run --ui.hideTopBar True --client.toolbarMode viewer main.py --server.port 80
